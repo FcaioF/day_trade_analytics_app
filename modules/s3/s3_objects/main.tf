081ecb899b4af3e4ce58336ff0355857c3100caf
Caio Francisco <caiofranciscoferreira@outlook.com>
2025-11-18 21:02:25 -0300

    pasta de trabalho s3

---
+++ modules/s3/s3_objects/main.tf
@@ -0,0 +1,21 @@
+# Pastas de trabalho do S3
+
+# python_scripts
+resource "aws_s3_object" "python_scripts" {
+  for_each = fileset("${var.app_bucket}/", "**")
+  bucket = var.name_bucket
+  key    = "pipeline/${each.value}"
+  source = "${var.app_bucket}/${each.value}"
+  etag   = filemd5("${var.app_bucket}/${each.value}")
+}
+
+# bash_scripts
+resource "aws_s3_object" "bash_scripts" {
+  for_each = fileset("${var.files_bash}/", "**")
+  bucket = var.name_bucket
+  key    = "scripts/${each.value}"
+  source = "${var.files_bash}/${each.value}"
+  etag   = filemd5("${var.files_bash}/${each.value}")
+}
+
+

711a30aedd79fcf479ad80b2afdc2295a21e61d8
Caio Francisco <caiofranciscoferreira@outlook.com>
2025-11-26 20:30:58 -0300

    alterações na politica que permite que maquina ec2 leia bucket s3

--- iac/modules/iam/main.tf
+++ iac/modules/iam/main.tf
@@ -1,33 +1,32 @@
 # Configuração dos recursos IAM
 
 # IAM Role para EC2 Instance Profile
-resource "aws_iam_role" "iam_app_profile_role" {
-  
-  name = "iam_app_profile_role"
+resource "aws_iam_policy" "s3_read_access_policy" {
+  name        = "s3-read-access-policy-${var.name_bucket}"
+  description = "Permite que a EC2 leia o bucket S3 de dados."
 
-  managed_policy_arns = [
-    "arn:aws:iam::aws:policy/service-role/AmazonElasticMapReduceforEC2Role"
-  ]
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2008-10-17",
-  "Statement": [
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:GetObject",
+          "s3:ListBucket",
+          "s3:GetBucketLocation",
+          "s3:GetObjectVersion"
+        ]
+        Effect   = "Allow"
+        Resource = [
+          "arn:aws:s3:::${var.name_bucket}",
+          "arn:aws:s3:::${var.name_bucket}/*"
+        ]
       },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
+    ]
+  })
 }
 
-# Profile
-resource "aws_iam_instance_profile" "app_profile" {
-  name = "app_profile"
-  role = aws_iam_role.iam_app_profile_role.name
+# 4. Anexa a Política de S3 à Role da EC2
+resource "aws_iam_role_policy_attachment" "app_profile" {
+  role       = aws_iam_role.iam_app_profile_role.name
+  policy_arn = aws_iam_policy.s3_read_access_policy.arn
 }

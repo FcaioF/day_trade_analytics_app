e2e66496eca32dbd822e5fa4f8645a5971dd46f1
Caio Francisco <caiofranciscoferreira@outlook.com>
2025-11-26 21:53:16 -0300

    mudança no  valor do parametro protocol

--- iac/modules/security-group/main.tf
+++ iac/modules/security-group/main.tf
@@ -28,7 +28,7 @@ resource "aws_security_group" "sg_allow_http" {
     description = "Inbound Rule"
     from_port = 22
     to_port = 22
-    protocol = "ssh"
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
 
   }

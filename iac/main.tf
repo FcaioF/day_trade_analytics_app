b4f6d826be7c5f6113135ec253d29009d5ab71b0
Caio Francisco <caiofranciscoferreira@outlook.com>
2025-11-27 19:17:46 -0300

    carrega arquivos para a pasta tmp

--- iac/main.tf
+++ iac/main.tf
@@ -30,7 +30,7 @@ module "dsa_ec2_instances" {
   vpc_security_group_ids = [module.sg_permite_http.security_group_id]
   user_data        = <<-EOF
                     #!/bin/bash           
-                    aws s3 cp s3://${var.name_bucket}/scripts/ /$HOME/scripts/ --recursive
+                    aws s3 cp s3://${var.name_bucket}/scripts/ /tmp/scripts/ --recursive
                     chmod +x /$HOME/scripts/bash_file.sh
                     /$HOME/scripts/bash_file.sh
 EOF
